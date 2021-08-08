581297fa79058ebe3e3c5d442cb4455ef6ed7d56
Rona Chan <45019584+ronachan@users.noreply.github.com>
2021-08-08 03:43:44 -0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React, {useState } from 'react';
 import './App.css';
-import Button from "./components/CustomButtonComponent";
+import Button from "./CustomButtonComponent";
 
 const App = () => {
   const [title, setTitle] = useState("");
